4163570ef60da7f90a01ba28ec8cd5c37dd7bcf3
oki <oki@netbsd.org>
1996-05-05 12:17:26 +0000

    NetBSD/x68k, by Masaru Oki.

---
+++ sys/arch/x68k/include/frame.h
@@ -0,0 +1,3 @@
+/*	$NetBSD: frame.h,v 1.1.1.1 1996/05/05 12:17:11 oki Exp $	*/
+
+#include <m68k/frame.h>
---
+++ sys/arch/x68k/include/psl.h
@@ -0,0 +1,3 @@
+/*	$NetBSD: psl.h,v 1.1.1.1 1996/05/05 12:17:11 oki Exp $	*/
+
+#include <m68k/psl.h>
