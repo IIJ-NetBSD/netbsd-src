598bcaf314ae3890563808c37c10b0e05464af5a
thorpej <thorpej@netbsd.org>
2003-01-18 06:37:06 +0000

    Merge the nathanw_sa branch.

--- sys/arch/sh3/include/mcontext.h
+++ sys/arch/sh3/include/mcontext.h
@@ -1,11 +1,11 @@
-/*	$NetBSD: altivec.h,v 1.6 2003/01/18 06:23:29 thorpej Exp $	*/
+/*	$NetBSD: mcontext.h,v 1.2 2003/01/18 06:33:41 thorpej Exp $	*/
 
 /*-
- * Copyright (c) 1999 The NetBSD Foundation, Inc.
+ * Copyright (c) 2001 The NetBSD Foundation, Inc.
  * All rights reserved.
  *
  * This code is derived from software contributed to The NetBSD Foundation
- * by Matt Thomas.
+ * by Klaus Klein.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -17,8 +17,8 @@
  *    documentation and/or other materials provided with the distribution.
  * 3. All advertising materials mentioning features or use of this software
  *    must display the following acknowledgement:
- *	This product includes software developed by the NetBSD
- *	Foundation, Inc. and its contributors.
+ *        This product includes software developed by the NetBSD
+ *        Foundation, Inc. and its contributors.
  * 4. Neither the name of The NetBSD Foundation nor the names of its
  *    contributors may be used to endorse or promote products derived
  *    from this software without specific prior written permission.
@@ -36,23 +36,52 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef	_POWERPC_ALTIVEC_H_
-#define	_POWERPC_ALTIVEC_H_
+#ifndef _SH3_MCONTEXT_H_
+#define _SH3_MCONTEXT_H_
 
-#define	VSCR_SA		0x00000001	/* Saturation happended (sticky) */
-#define	VSCR_NJ		0x00010000	/* Non Java-IEEE-C9X FP mode */
+/*
+ * Layout of mcontext_t for the sh3 architecture.
+ */
 
-#ifdef _KERNEL
-void enable_vec(void);
-void save_vec_cpu(void);
-void save_vec_lwp(struct lwp *);
-#ifdef MULTIPROCESSOR
-void mp_save_vec_lwp(struct lwp *);
-#endif
-void init_vec(void);
-void vzeropage(paddr_t);
-void vcopypage(paddr_t, paddr_t);	/* dst, src */
-extern struct pool vecpool;
-#endif
+#define _NGREG		22
+typedef int		__greg_t;
+typedef __greg_t	__gregset_t[_NGREG];
 
-#endif	/* _POWERPC_ALTIVEC_H_ */
+#define	_REG_EXPEVT	0
+#define	_REG_PC		1
+#define	_REG_SR		2
+#define	_REG_MACL	3
+#define	_REG_MACH	4
+#define	_REG_PR		5
+#define	_REG_R14	6
+#define	_REG_R13	7
+#define	_REG_R12	8
+#define	_REG_R11	9
+#define	_REG_R10	10
+#define	_REG_R9		11
+#define	_REG_R8		12
+#define	_REG_R7		13
+#define	_REG_R6		14
+#define	_REG_R5		15
+#define	_REG_R4		16
+#define	_REG_R3		17
+#define	_REG_R2		18
+#define	_REG_R1		19
+#define	_REG_R0		20
+#define	_REG_R15	21
+/* Convenience synonym */
+#define	_REG_SP		_REG_R15
+
+typedef struct {
+	int		__fpr_fpscr;
+	double		__fpr_regs[8];
+} __fpregset_t;
+
+typedef struct {
+	__gregset_t	__gregs;
+	__fpregset_t	__fpregs;
+} mcontext_t;
+
+#define	_UC_MACHINE_SP(uc)	((uc)->uc_mcontext.__gregs[_REG_SP])
+
+#endif /* !_SH3_MCONTEXT_H_ */
