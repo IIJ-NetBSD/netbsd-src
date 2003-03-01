ea7fe0519d36356688307b9a648d959ee3d09b72
mrg <mrg@netbsd.org>
2003-03-01 12:26:36 +0000

    merge binutils 2.13.2.1

--- gnu/dist/toolchain/ld/emulparams/shelf_nbsd.sh
+++ gnu/dist/toolchain/ld/emulparams/shelf_nbsd.sh
@@ -1,11 +1,11 @@
-SCRIPT_NAME=elf
+# If you change this file, please alsolook at files which source this one:
+# shlelf_nbsd.sh
+
+. ${srcdir}/emulparams/shelf.sh
+
 OUTPUT_FORMAT="elf32-sh-nbsd"
 TEXT_START_ADDR=0x400000
 MAXPAGESIZE=0x10000
-ARCH=sh
-MACHINE=
-TEMPLATE_NAME=elf32
-GENERATE_SHLIB_SCRIPT=yes
 
 DATA_START_SYMBOLS='__data_start = . ;';
 
